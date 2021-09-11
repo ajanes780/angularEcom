d8cae7c17e571b7a1c1104137666aae97cdce02a
Aaron Janes <aaron.janes@gmail.com>
2021-09-11 10:52:33 -0600

    added console.log colors

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const mongoose = require('mongoose')
+const colors = require('colors/safe')
 const api = process.env.API_URL
 const app = express()
 
@@ -27,6 +28,13 @@ app.post(`${api}/products`, (req, res) => {
   res.send('Hello World')
 })
 
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGOUSER}:${process.env.MONGOPASS}@angularstore.irh8h.mongodb.net/angularstore?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    console.log(colors.rainbow('MongoDb Connected'))
+  })
 app.listen(3000, () => {
-  console.log(' Server is running on port 3000')
+  console.log(colors.random('Server is running on port 3000'))
 })
