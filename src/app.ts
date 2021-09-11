58c6b380ed0304472b9ac173880c399e32deb98f
Aaron Janes <aaron.janes@gmail.com>
2021-09-11 11:46:37 -0600

    set up mongoose. seeded db and tested routes

--- src/app.ts
+++ src/app.ts
@@ -11,30 +11,53 @@ const app = express()
 app.use(express.json())
 app.use(morgan('tiny'))
 
-// routes
-app.get(`${api}/products`, (req, res) => {
-  const product = {
-    id: 1,
-    name: 'string',
-    url: 'nome_url',
-  }
-  res.send(product)
+
+const productSchema = mongoose.Schema({
+  id: Number,
+  name: String,
+  image: String,
+  countInStock: Number,
 })
 
-app.post(`${api}/products`, (req, res) => {
-  const newProduct = req.body
+const Product = mongoose.model('Product', productSchema)
+// routes
+app.get(`${api}/products`, async (req, res) => {
+  const productList = await Product.find()
+  if (!productList) {
+    res.status(500).json({ success: false })
+  }
+  res.send(productList)
+})
 
-  console.log(`newProduct`, newProduct)
-  res.send('Hello World')
+app.post(`${api}/products`, async (req, res) => {
+  const product = new Product({
+    id: req.body.id,
+    name: req.body.name,
+    image: req.body.image,
+    countInStock: req.body.countInStock,
+  })
+  try {
+    const createdProduct = await product.save()
+    console.log(`createdProduct`, createdProduct)
+    res.status(201).json(createdProduct)
+    console.log(colors.inverse(`Success this product was created`, product))
+  } catch (error) {
+    res.status(500).json({ message: error, success: false })
+    console.log(colors.red(error))
+  }
 })
 
 mongoose
   .connect(
-    `mongodb+srv://${process.env.MONGOUSER}:${process.env.MONGOPASS}@angularstore.irh8h.mongodb.net/angularstore?retryWrites=true&w=majority`
+    `mongodb+srv://${process.env.MONGOUSER}:${process.env.MONGOPASS}@angularstore.irh8h.mongodb.net/angularStore?retryWrites=true&w=majority`
   )
   .then(() => {
-    console.log(colors.rainbow('MongoDb Connected'))
+    console.log(colors.rainbow('MongoDB is now connected'))
   })
+  .catch((e) => {
+    console.log(colors.red(` There has been a database error ${e}`))
+  })
+  
 app.listen(3000, () => {
   console.log(colors.random('Server is running on port 3000'))
 })
