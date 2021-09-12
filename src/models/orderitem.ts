08713ac456633361c8849ee5b7bc446ca9581191
Aaron Janes <aaron.janes@gmail.com>
2021-09-12 11:13:57 -0600

    Created order and order Items schema

---
+++ src/models/orderitem.ts
@@ -0,0 +1,9 @@
+export {};
+const mongoose = require("mongoose");
+
+const orderItem = mongoose.Schema({
+  quantity: { type: Number, required: true },
+  product: { type: mongoose.Schema.Types.ObjectId, ref: "Product" },
+});
+
+module.exports = mongoose.model("Orders", orderItem);
--- src/models/orders.ts
+++ src/models/orders.ts
@@ -1,7 +1,23 @@
 export {}
 const mongoose = require('mongoose')
 
-const ordersSchema = mongoose.Schema({})
+const ordersSchema = mongoose.Schema({
+  orderItems: [{ type: mongoose.Schema.Types.ObjectId, ref: "OrderItem", required: true }],
+  shippingAddress1: { type: String, required: true },
+  shippingAddress2: { type: String, default: "" },
+  city: { type: String, required: true },
+  postalCode: { type: String, required: true },
+  Country: { type: String, required: true },
+  phone: { type: String, required: true },
+  status: { type: String, required: true, default: "Pending" },
+  totalPrice: { type: Number, required: true },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  dateOrdered: { type: Date, default: Date.now },
+});
+
+
+
+
 ordersSchema.virtual("id").get(function () {
   return this._id.toHexString();
 });
