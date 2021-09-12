7c1773e2cb28aead41dea8b60f582b34e4596fa4
Aaron Janes <aaron.janes@gmail.com>
2021-09-11 20:51:33 -0600

    get all users , get by Id and add user completed

--- src/models/users.ts
+++ src/models/users.ts
@@ -1,13 +1,24 @@
 export {}
 const mongoose = require('mongoose')
 
-const usersSchema = mongoose.Schema({})
+const usersSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  passwordHash: { type: String, required: true },
+  phone: { type: String, required: true },
+  isAdmin: { type: Boolean, required: true },
+  street: { type: String, default: "" },
+  apartment: { type: String, default: "" },
+  city: { type: String, default: "" },
+  postalCode: { type: String, default: "" },
+  country: { type: String, default: "" },
+});
 
- usersSchema.virtual("id").get(function () {
-   return this._id.toHexString();
- });
+usersSchema.virtual("id").get(function () {
+  return this._id.toHexString();
+});
 
- usersSchema.set("toJSON", {
-   virtuals: true,
- });
+usersSchema.set("toJSON", {
+  virtuals: true,
+});
 module.exports = mongoose.model('Users', usersSchema)
--- src/routers/users.ts
+++ src/routers/users.ts
@@ -3,17 +3,110 @@ const router = express.Router()
 const User = require('../models/users')
 import { Request, Response } from 'express'
 const colors = require('colors/safe')
+const bcrypt = require("bcryptjs");
+const mongoose = require("mongoose");
 
-router.get(`/`, async (eq: Request, res: Response) => {
+// get all of users
+router.get(`/`, async (req: Request, res: Response) => {
   try {
-    const usersList = await User.find()
-    res.send(usersList)
+    const usersList = await User.find().select("-passwordHash");
+    res.send(usersList);
   } catch (error) {
-    console.log(colors.red(error))
-    res.status(500).json({ success: false, message: error })
+    console.log(colors.red(error));
+    res.status(500).json({ success: false, message: error });
   }
-})
+});
+//  get on single user
+router.get(`/:id`, async (req: Request, res: Response) => {
+  const { id } = req.params;
+  if (!mongoose.isValidObjectId(id)) {
+    res.status(400).send("Invalid user id");
+  }
+  try {
+    const user = await User.findById(id).select("-passwordHash");
+    console.log(`user`, user);
+    res.send(user);
+  } catch (error) {
+    console.log(colors.red(error));
+    res.status(500).json({ success: false, message: error });
+  }
+});
 
-router.post('/', async (req: Request, res: Response) => {})
+// add a new user
+router.post("/", async (req: Request, res: Response) => {
+  const { name, email, password, phone, isAdmin, street, apartment, city, postalCode, country } = req.body;
+  const checkEmail = await User.find({ email });
+
+  if (checkEmail.length) return res.send("That user already exists");
+
+  try {
+    let newUser = new User({
+      name,
+      email,
+      passwordHash: bcrypt.hashSync(password, 8),
+      phone,
+      isAdmin,
+      street,
+      apartment,
+      city,
+      postalCode,
+      country,
+    });
+    const result = await newUser.save();
+
+    if (result) {
+      res.status(201).json({ result, success: true });
+    }
+  } catch (error) {
+    res.status(404).json({
+      message: "The user could not be created",
+      success: false,
+      error: error,
+    });
+  }
+});
+
+// update user
+router.put("/:id", async (req: Request, res: Response) => {
+  const { name, email, password, phone, isAdmin, street, apartment, city, postalCode, country } = req.body;
+  const { id } = req.params;
+  if (!mongoose.isValidObjectId(id)) {
+    res.status(400).send("Invalid user id");
+  }
+
+  const userExists = await User.findById(id);
+  if (!userExists) return res.send("User not found");
+  let newPassword: string;
+
+  if (password) {
+    newPassword = bcrypt.hashSync(password, 8);
+  } else {
+    newPassword = userExists.passwordHash;
+  }
+
+  try {
+    let updateUser = await User.findByIdAndUpdate(id, {
+      name: name || userExists.name,
+      email: email || userExists.email,
+      passwordHash: newPassword,
+      phone: phone || userExists.phone,
+      isAdmin: isAdmin || userExists.isAdmin,
+      street: street || userExists.street,
+      apartment: apartment || userExists.apartment,
+      city: city || userExists.city,
+      postalCode: postalCode || userExists.postalCode,
+      country: country || userExists.country,
+    });
+    if (updateUser) {
+      res.status(201).json({ updateUser, message: "User has been updated", success: true });
+    }
+  } catch (error) {
+    res.status(404).json({
+      message: "The user could not be updated",
+      success: false,
+      error: error,
+    });
+  }
+});
 
 module.exports = router
